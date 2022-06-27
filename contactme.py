ee5a1dae0ac07e2377be8f7e023e430ab141e7b1
Alon-Dell <alon.shtrikman@gmail.com>
2022-06-27 18:59:24 +0300

    f

--- contactme.py
+++ contactme.py
@@ -4,6 +4,14 @@ st.set_page_config(
     page_title="Contact Me",
     layout='centered',
     )
+
+hide_streamlit_style = """
+            <style>
+            
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 # %%
 st.markdown(f"<h1 style='color:#38afff'>Hi :)</h1>",unsafe_allow_html=True)
 st.markdown(f"#### My name is Alon Shtrikman")
@@ -14,16 +22,16 @@ st.markdown(f"#### If you found my suitcase, please contact me using the followi
 def im(id):
     return "http://drive.google.com/uc?export=view&id="+str(id)
 
-def mkdn(link,img):
+def mkdn(link,img,name):
     return f"""
-            <a href="{link}">
-            <img src="{img}" width="100" height="100" class="center"/>
+            <a href="{link}" style="font-size: 25px; text-decoration: none">
+            <img src="{img}" width="70" height="70" class="center"/>
+            {name}
             </a>
             """
 
 
 col1, col2 = st.columns(2)
-sp=st.markdown("---")
 col3,col4 = st.columns(2)
 
 wa_link=r"https://wa.me/972547592228?text=Hi%252C%20I%20found%20your%20suitcase"
@@ -36,17 +44,17 @@ phone_link=r"tel:00972547592228"
 phone_photo=im('1lVV0IU4AJhn5L2CXv7FaKQmc9DinZwD3')
 
 
-col1.markdown(f"### [WhatsApp]({wa_link})",unsafe_allow_html=True)
-col1.markdown(mkdn(wa_link,wa_photo),unsafe_allow_html=True)
+# col1.markdown(f"### [WhatsApp]({wa_link})",unsafe_allow_html=True)
+col1.markdown(mkdn(wa_link,wa_photo,'WhatsApp'),unsafe_allow_html=True)
 
 
-col2.markdown(f"### [Facebook Messenger]({fb_link})",unsafe_allow_html=True)
-col2.markdown(mkdn(fb_link,fb_photo),unsafe_allow_html=True)
+# col2.markdown(f"### [Facebook Messenger]({fb_link})",unsafe_allow_html=True)
+col2.markdown(mkdn(fb_link,fb_photo,'Facebook Messanger'),unsafe_allow_html=True)
 
-col3.markdown(f"### [Email]({mail_link})",unsafe_allow_html=True)
-col3.markdown(mkdn(mail_link,mail_photo),unsafe_allow_html=True)
+# col3.markdown(f"### [Email]({mail_link})",unsafe_allow_html=True)
+col3.markdown(mkdn(mail_link,mail_photo,'Email'),unsafe_allow_html=True)
 
-col4.markdown(f"### [+972-54-7592228]({phone_link})",unsafe_allow_html=True)
-col4.markdown(mkdn(phone_link,phone_photo),unsafe_allow_html=True)
+# col4.markdown(f"### [+972-54-7592228]({phone_link})",unsafe_allow_html=True)
+col4.markdown(mkdn(phone_link,phone_photo,'+972-54-759-2228'),unsafe_allow_html=True)
 
 
